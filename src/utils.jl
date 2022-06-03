79f576478bb3a4c0cee228cf067c3dd413b0c88c
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-06-03 18:14:46 +0200

    utils: hexcount

--- src/utils.jl
+++ src/utils.jl
@@ -2,6 +2,8 @@ const hsq3 = sqrt(3) / 2
 const sq3 = sqrt(3)
 const d_unit_area = sqrt(2 / sqrt(3))
 
+hexcount(n) = 3 * n * (n + 1) + 1 # https://oeis.org/A003215
+
 function hexagon_vertices(x, y, d)
     w = d / sq3
     (x - w    , y), 
