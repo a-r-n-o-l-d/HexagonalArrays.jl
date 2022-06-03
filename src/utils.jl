57d49b769f194fddf1690f6a09a25efb430f2e64
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-06-03 18:04:19 +0200

    utils: hexagon_vertices

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,13 @@
 const hsq3 = sqrt(3) / 2
 const sq3 = sqrt(3)
 const d_unit_area = sqrt(2 / sqrt(3))
+
+function hexagon_vertices(x, y, d)
+    w = d / sq3
+    (x - w    , y), 
+    (x - w / 2, y + d / 2), 
+    (x + w / 2, y + d / 2),
+    (x + w    , y),
+    (x + w / 2, y - d / 2),
+    (x - w / 2, y - d / 2)
+end
