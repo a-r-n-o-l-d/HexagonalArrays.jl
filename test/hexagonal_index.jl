0e0fed6e2029b4f0cac970ee72a54c629a84f445
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-06-03 17:58:22 +0200

    HexagonalIndex to_cartesian

--- test/hexagonal_index.jl
+++ test/hexagonal_index.jl
@@ -4,4 +4,6 @@
     @test one(HexagonalIndex) + one(HexagonalIndex) == 2 * one(HexagonalIndex)
 
     @test one(HexagonalIndex) - one(HexagonalIndex) == zero(HexagonalIndex)
+    
+    @test(0.0, 0.0) == to_cartesian(one(HexagonalIndex), 1)
 end
