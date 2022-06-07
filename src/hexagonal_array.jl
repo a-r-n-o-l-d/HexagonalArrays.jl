71fdad7775c3b3ca5c904301ab2978b8d3845212
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-06-07 20:32:32 +0200

    housekeeping

--- src/hexagonal_array.jl
+++ src/hexagonal_array.jl
@@ -12,11 +12,11 @@ Base.size(A::HexagonalArray) = size(A.data)
 
 Base.@propagate_inbounds Base.getindex(A::HexagonalArray, I::HexagonalIndex) = A.data[I.I...]
 
-Base.@propagate_inbounds Base.setindex!(H::HexagonalArray, v, I::HexagonalIndex) = (H.data[I.I...] = v)
+Base.@propagate_inbounds Base.setindex!(A::HexagonalArray, v, I::HexagonalIndex) = (A.data[I.I...] = v)
 
-Base.@propagate_inbounds Base.getindex(H::HexagonalArray, idxs::Int...) = H.data[idxs...]
+Base.@propagate_inbounds Base.getindex(A::HexagonalArray, idxs::Int...) = A.data[idxs...]
 
-Base.@propagate_inbounds Base.setindex!(H::HexagonalArray, v, idxs::Int...) = (H.data[idxs...] = v)
+Base.@propagate_inbounds Base.setindex!(A::HexagonalArray, v, idxs::Int...) = (A.data[idxs...] = v)
 
 hexzeros(T::Type, nrow, ncol, d_unit) = HexagonalArray(zeros(T, nrow, ncol, 2), d_unit)
 
@@ -25,30 +25,3 @@ hexzeros(nrow, ncol, d_unit) = HexagonalArray(zeros(Float64, nrow, ncol, 2), d_u
 hexones(T::Type, nrow, ncol, d_unit) = HexagonalArray(ones(T, nrow, ncol, 2), d_unit)
 
 hexones(nrow, ncol, d_unit) = HexagonalArray(ones(Float64, nrow, ncol, 2), d_unit)
-
-#Base.getindex(H::HexagonalArray, i::Int) = data(H)[i]
-
-#Base.getindex(H::HexagonalArray, I::Vararg{Int,3}) = data(H)[I...]
-
-#Base.getindex(H::HexagonalArray) = data(H)[]
-
-#Base.getindex(H::HexagonalArray, c::Vararg{Union{Colon, Int},3}) = datas(H)[c...]
-    #parent_call(x -> getindex(x, c...), A)
-#=
-function Base.setindex!(H::HexagonalArray, val, I::HexagonalIndex)
-    datas(H)[I.I...] = val
-    H
-end
-
-function Base.setindex!(H::HexagonalArray, val, i::Int)
-    datas(H)[i] = val
-    H
-end
-
-function Base.setindex!(H::HexagonalArray, val, I::Vararg{Int,3})
-    datas(H)[I...] = val
-    H
-end
-=#
-
-
