9b1e624cca861f13a4538afd658ddcef291433ef
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-06-09 17:44:54 +0200

    housekeeping

--- src/hexagonal_index.jl
+++ src/hexagonal_index.jl
@@ -19,7 +19,7 @@ Base.one(::Type{HexagonalIndex}) = HexagonalIndex(1, 1, 1) #HexagonalIndex(1, 1,
 Base.:(==)(I1::HexagonalIndex, I2::HexagonalIndex) = I1.I == I2.I
 
 # Addition
-function Base.:+(I1::HexagonalIndex, I2::HexagonalIndex)
+function Base.:(+)(I1::HexagonalIndex, I2::HexagonalIndex)
     i1, j1, k1 = Tuple(I1)
     i2, j2, k2 = Tuple(I2)
     k1 = Bool(k1 - 1)
@@ -30,16 +30,16 @@ function Base.:+(I1::HexagonalIndex, I2::HexagonalIndex)
 end
 
 # Negation
-function Base.:-(I::HexagonalIndex)
+function Base.:(-)(I::HexagonalIndex)
     i, j, k = Tuple(I)
     HexagonalIndex(-i - k + 1, -j - k + 1, k)
 end
 
 # Subtraction
-Base.:-(I1::HexagonalIndex, I2::HexagonalIndex) = I1 + (-I2)
+Base.:(-)(I1::HexagonalIndex, I2::HexagonalIndex) = I1 + (-I2)
 
 # Scalar multiplication 
-function Base.:*(s::Int, I::HexagonalIndex)
+function Base.:(*)(s::Int, I::HexagonalIndex)
     s < 0 && return (-s) * (-I)
     i, j, k = Tuple(I)
     HexagonalIndex(i * s + (k - 1) * (s >> 1),
@@ -47,7 +47,7 @@ function Base.:*(s::Int, I::HexagonalIndex)
                    mod(s * (k - 1), 2) + 1)
 end
 
-Base.:*(I::HexagonalIndex, s::Int) = *(s::Int, I::HexagonalIndex)
+Base.:(*)(I::HexagonalIndex, s::Int) = *(s::Int, I::HexagonalIndex)
 
 # Cartesian coordinates => utils
 function to_cartesian(I::HexagonalIndex, d_unit) #to_cartesian(I::HexagonalIndex, H::HexagonalArray)
