08505925f3fa85096b45371a3dbbbee3237136b0
Documenter.jl <documenter@juliadocs.github.io>
2022-06-10 15:42:33 +0000

    build based on 09a0093

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HexagonalArrays","category":"page"},{"location":"#HexagonalArrays","page":"Home","title":"HexagonalArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HexagonalArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HexagonalArrays]","category":"page"}]
+}
