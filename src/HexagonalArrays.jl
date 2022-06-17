87ef290f32a23888660033905d571128e2894711
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-06-17 20:53:43 +0200

    Move HexagonalNeighborhood to its own package

--- src/HexagonalArrays.jl
+++ src/HexagonalArrays.jl
@@ -9,9 +9,6 @@ include("hexagonal_index.jl")
 export HexagonalArray, harray, hzeros, hones
 include("hexagonal_array.jl")
 
-export NeighborhoodIndex, HexagonalNeighborhood, @neighborhood
-include("hexagonal_neighborhood.jl")
-
 export Replicate, Constant, Periodic, Symmetric
 include("extrapolation.jl")
 
